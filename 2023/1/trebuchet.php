0a024a0635a4f739d9ed09c48705e0ed38bfe267
Sander Camp <sander.camp@kabisa.nl>
2023-12-01 15:44:10 +0100

    Day 1 - Puzzle 2

--- 2023/1/trebuchet.php
+++ 2023/1/trebuchet.php
@@ -2,10 +2,32 @@
 
 (function() {
     $lines = file('2023/1/input.txt');
-    $range = range(1, 9);
-    $numbers = array_map(
-        fn(string $line) => array_values(array_filter(str_split($line), fn(string $char) => in_array($char, $range))),
+
+    $map = [
+        'ni9ne' => 'nine',
+        'ei8ght' => 'eight',
+        'se7ven' => 'seven',
+        'si6x' => 'six',
+        'f5ive' => 'five',
+        'f4our' => 'four',
+        'th3ree' => 'three',
+        't2wo' => 'two',
+        'o1ne' => 'one'
+    ];
+
+    $parsedLines = array_map(
+        fn (string $line) => str_replace(
+            array_values($map),
+            array_keys($map),
+            $line
+        ),
         $lines
+    );
+
+    $integers = range(1, 9);
+    $numbers = array_map(
+        fn(string $line) => array_values(array_filter(str_split($line), fn(string $char) => in_array($char, $integers))),
+        $parsedLines
     );
 
     $filtered = array_map(
