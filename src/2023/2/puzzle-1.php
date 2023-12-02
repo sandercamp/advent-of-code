b75e553fdf8b79b8a14fae70b878b6a20f4f82bd
Sander Camp <sander.camp@kabisa.nl>
2023-12-02 09:00:45 +0100

    Remove bloat

--- src/2023/2/puzzle-1.php
+++ src/2023/2/puzzle-1.php
@@ -7,14 +7,13 @@ $parsedInput = parseInput();
 $colors = ['green' => 13, 'red' => 12, 'blue' => 14];
 $result = 0;
 foreach ($parsedInput as $id => $game) {
-    $validColors = [];
     foreach ($colors as $color => $max) {
-        $validColors[$color] = count(array_filter(array_column($game, $color), fn($i) => $i > $max)) === 0;
+        if (count(array_filter(array_column($game, $color), fn($i) => $i > $max)) !== 0) {
+            continue 2;
+        }
     }
 
-    if (!in_array(false, $validColors, true)) {
-        $result += $id;
-    }
+    $result += $id;
 }
 
 var_dump($result); // 2416
