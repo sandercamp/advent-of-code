8f5c686d91aeb4dfeadeb965e31dd4441afa4549
Sander Camp <sander.camp@kabisa.nl>
2023-12-02 08:51:13 +0100

    Less is more

--- src/2023/2/puzzle-2.php
+++ src/2023/2/puzzle-2.php
@@ -5,15 +5,10 @@ require_once('parse-input.php');
 $parsedInput = parseInput();
 
 $result = 0;
-foreach ($parsedInput as $game) {
-    $green = array_column($game, 'green');
-    $red = array_column($game, 'red');
-    $blue = array_column($game, 'blue');
-    rsort($green);
-    rsort($red);
-    rsort($blue);
+$colors = ['green', 'red', 'blue'];
 
-    $result += reset($red) * reset($green) * reset($blue);
+foreach ($parsedInput as $game) {
+    $result += array_product(array_map(fn ($color) => max(array_column($game, $color)), $colors));
 }
 
 var_dump($result); // 63307
