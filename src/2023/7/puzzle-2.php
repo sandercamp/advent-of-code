5df9eb3266e890645b393791ac8cb3774f4ddaf7
Sander Camp <sander.camp@kabisa.nl>
2023-12-07 12:44:07 +0100

    Clean up

--- src/2023/7/puzzle-2.php
+++ src/2023/7/puzzle-2.php
@@ -5,19 +5,15 @@ require_once('helpers.php');
 
 run(
     function() {
-        //var_dump(determineTypeTwo('J243T'));die;
-
         $parsedInput = parseInputTwo();
 
         usort($parsedInput, 'compareHands');
-
-        var_dump(array_reverse($parsedInput));
 
         $result = 0;
         foreach ($parsedInput as $rank => [$hand, $type, $bid]) {
             $result += $bid * ($rank + 1);
         }
 
-        echo "Result: {$result}\n"; // Test: 5905 | Input:250755480 too low
+        echo "Result: {$result}\n"; // Test: 5905 | Input: 250757288
     }
 );
