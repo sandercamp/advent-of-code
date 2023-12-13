d59a1cfe7f59d40fbd515050b0c2a6e4515e6113
Sander Camp <sander.camp@kabisa.nl>
2023-12-13 16:57:37 +0100

    Day 12 - Almost there...

--- src/2023/12/helpers.php
+++ src/2023/12/helpers.php
@@ -9,11 +9,16 @@ function parseInput(): array {
 
         $groups = [];
         $brokenSprings = 0;
-        foreach (explode(',', $record) as $group) {
-            $brokenSprings += (int)$group;
-            $groups[] = (int)$group;
-        }
 
+        $groups = explode(',', $record);
+
+        for ($i = 0; $i < count($groups); $i++) {
+            $length = (int)$groups[$i];
+            $brokenSprings += $length;
+            $pattern = str_repeat('#', $length);
+            $isLast = !isset($groups[$i + 1]);
+            $groups[$i] = $isLast ? $pattern : sprintf('%s.', $pattern);
+        }
 
         $input[] = [$springs, $groups, $brokenSprings];
     }
@@ -21,12 +26,102 @@ function parseInput(): array {
     return $input;
 }
 
-function generate(string $string, array $chars, array $positions) {
+function generate(string $string, array $groups, $offset = 0): int {
+    $matches = [];
+
+    $total = 0;
+    foreach ($groups as $group) {
+        $total += strlen($group);
+    }
+
+    $l = strlen($string);
+    for ($i = 0; $i <= $l; $i++) {
+        $offset = $i;
+        $remainder = $total;
+        for ($g = 0; $g < count($groups); $g++) {
+            $subString = $groups[$g];
+
+            if ($remainder < available(substr($string, $offset))) {
+                continue;
+            }
+
+            if (isMatch(substr($string, $offset, strlen($subString)), $subString)) {
+                $matches[$g][$offset] = $subString;
+            }
+
+            $offset = $offset + strlen($subString);
+            $remainder = $remainder - strlen($subString);
+        }
+    }
+
+    for ($m = 0; $m < count($matches) - 1; $m++) {
+        foreach ($matches[$m] as $offset => $match) {
+            if (overlaps($matches[$m + 1], [$offset, $match])) {
+                unset($matches[$m][$offset]);
+            }
+        }
+    }
+
+    var_dump($matches);die;
+
+    return array_reduce(
+        $matches,
+        fn(int $carry, array $matches) => $carry * count($matches),
+        1
+    );
+}
+
+function available(string $string): int {
+    $unknown = substr_count($string, '?');
+    $broken = substr_count($string, '#');
+
+    return $unknown + $broken;
+}
+
+
+function overlaps(array $matches, array $match): bool {
+    [$offsetA, $stringA] = $match;
+
+    $x1 = $offsetA;
+    $x2 = $x1 + strlen($stringA);
+    foreach ($matches  as $offsetB => $stringB) {
+        $y1 = $offsetB;
+        $y2 = $y1 + strlen($stringB);
+
+        if ($x1 < $y2 && $y1 < $x2) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function isMatch(string $a, string $b): bool {
+    if ($a === $b) {
+        return true;
+    }
+
+    if (strlen($a) === strlen($b)) {
+        for ($i = 0; $i < strlen($b); $i++) {
+            if (!($a[$i] === '?' || $a[$i] === $b[$i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+
+function generateB(string $string, array $chars, array $positions) {
     $result = [];
 
     foreach (['.', '#'] as $char) {
         foreach ($positions as $key => $position) {
             $permutation = substr_replace($string, $char, $position, 1);
+
 
             $otherPositions = $positions;
             unset($otherPositions[$key]);
--- src/2023/12/puzzle-1.php
+++ src/2023/12/puzzle-1.php
@@ -9,28 +9,7 @@ run(
 
         $result = 0;
         foreach ($input as [$springs, $groups, $brokenSprings]) {
-            $positions = [];
-
-            $pos = 0;
-            while (($pos = strpos($springs, '?', $pos)) !== false) {
-                $positions[] = $pos;
-                $pos++;
-            }
-
-            $strings = array_filter(
-                generate($springs, ['.', '#'], $positions),
-                function(string $string) use ($groups) {
-                    $parts = array_filter(explode('.', $string));
-
-                    $sum = array_values(array_map(fn(string $part) => substr_count($part, '#'), $parts));
-
-                    return strpos($string, '?') === false && $sum == $groups;
-                }
-            );
-
-            var_dump($strings);
-
-            $result += count($strings);
+            $result += generate($springs, $groups);
         }
 
         echo "Result: {$result}\n"; // Test: 20 | Input: ?
